87291fe9491d85f935b1a0633bb2cfbf7b8d875c
Nick Wade <nick@nwade.com>
2015-09-16 11:13:52 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# uservices-spring
+# uServices Spring Boot
+
+Evolution of a component based architecture using Spring Boot.
+
+See Git tags for step-by-step notes.
+
+```
+git tag -ln
+
+v1              First commit
+v2              Functional groups
+v3              Feature groups
+v4              Components
+v5              Services
+```
+
+## Database Example
+
+```
+mysql -uroot --execute="create database uservices_test"
+
+mysql -uroot --execute="grant all on uservices_test.* to 'uservices'@'localhost' identified by 'uservices';"
+```
+
---
+++ src/test/java/com/nwade/uservices/RegistrationControllerTest.java
@@ -0,0 +1,55 @@
+package com.nwade.uservices;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.sql.DataSource;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {App.class})
+@WebAppConfiguration
+public class RegistrationControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext applicationContext;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Before
+    public void setUp() throws Exception {
+        TestDataSource.cleanWithFixtures(dataSource);
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+        String json = "{\"name\":\"aUser\"}";
+
+        mockMvc.perform(
+                post("/registration")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(mvcResult -> {
+                    User actual = new ObjectMapper().readValue(mvcResult.getResponse().getContentAsString(), User.class);
+                    assertEquals("aUser", actual.getName());
+                });
+    }
+}
